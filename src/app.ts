bd238246009b7f72fe6aa8ff11bf3ed10908e4ff
rkh-leung <leptanian@gmail.com>
2021-07-21 23:54:16 +1200

    add gatherUserInput private function

--- src/app.ts
+++ src/app.ts
@@ -35,9 +35,28 @@ class ProjectInput {
         this.attach()
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.titleInputElement.value
+        const enteredPeople = this.titleInputElement.value
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0  ) {
+            alert('Invalid input, please try again')
+            return
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+
     @autobind()
     private submitHandler (event: Event) {
         event.preventDefault()
+        const userInput = this.gatherUserInput()
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput
+            console.log(title, desc, people)
+        }
     }
 
     private configure() {
