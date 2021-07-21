da0afb5f12b93c5ab8986789e79fadcf5388033b
rkh-leung <leptanian@gmail.com>
2021-07-21 23:34:56 +1200

    add submit event listener to input form

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+//autobind
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjdescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(): any {
+            return originalMethod.bind(this)
+        }
+    }
+    return adjdescriptor
+}
+
+// project input class
 class ProjectInput {
     templateElement: HTMLTemplateElement // accessible after enabling dom under lib in tsconfig
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
@@ -9,7 +25,23 @@ class ProjectInput {
 
         const importNode = document.importNode(this.templateElement.content, true)
         this.element = importNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
         this.attach()
+    }
+
+    @autobind()
+    private submitHandler (event: Event) {
+        event.preventDefault()
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
