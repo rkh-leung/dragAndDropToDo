109a5663616f4c19f7ca54b820968ad7500dff80
rkh-leung <leptanian@gmail.com>
2021-08-01 13:27:00 +1200

    add: getter to render conditional text

--- src/app.ts
+++ src/app.ts
@@ -147,6 +147,14 @@ abstract class BaseClass<T extends HTMLElement, U extends HTMLElement> {
 
 class ProjectItem extends BaseClass<HTMLUListElement, HTMLElement> {
   private project: Project
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person'
+    } else {
+      return `${this.project.people} persons`
+    }
+  }
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id)
     this.project = project
@@ -158,7 +166,7 @@ class ProjectItem extends BaseClass<HTMLUListElement, HTMLElement> {
   configure() {}
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title
-    this.element.querySelector('h3')!.textContent = this.project.people.toString()
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
     this.element.querySelector('p')!.textContent = this.project.description
   }
 }
