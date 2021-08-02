945f4f0038a811ef1c18380137393f9d29db01e4
rkh-leung <leptanian@gmail.com>
2021-08-02 17:30:03 +1200

    fix: webpack setup to build properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
   },
   devtool: 'inline-source-map',
   module: {
