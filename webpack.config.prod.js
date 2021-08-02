196a525953ebadbad2ea3f49fce82df5d16ca820
rkh-leung <leptanian@gmail.com>
2021-08-02 19:02:13 +1200

    add: webpack production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,16 @@
 const path = require('path')
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
   },
-  devtool: 'inline-source-map',
+  // devtool: '(none)',
   module: {
-    rules: [
+    rules: [  // per file level
       {
         test: /\.ts$/,
         use: 'ts-loader',
@@ -20,5 +20,8 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  plugins: [  // general workflow
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
 }
